4391ee9595b3007ffee2b0a756f636449b5ed825
Venkat0023 <41055353+Venkat0023@users.noreply.github.com>
2019-01-14 21:18:26 +0530

    Update Oddoreven.java

--- Oddoreven.java
+++ Oddoreven.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.io.*;
 
-public class Oddoreven {
+class Oddoreven {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a= scanner.nextInt();
