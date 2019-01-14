ac9d6160f88166104eabacb06185401093503d8e
Venkat0023 <41055353+Venkat0023@users.noreply.github.com>
2019-01-14 20:51:28 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,6 @@ import java.io.*;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-      System.out.println("Negative");
-
         Scanner scanner = new Scanner(System.in);
       int a= scanner.nextInt();
       if(a==0)
