83ab4f5da3b1c99fe82caedf3840a1e899765a83
Venkat0023 <41055353+Venkat0023@users.noreply.github.com>
2019-01-14 21:13:55 +0530

    Add files via upload

---
+++ Oddoreven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Oddoreven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a= scanner.nextInt();
+        if(a<0)
+            System.out.println("Invalid");
+        else if (a%2==0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+
+    }
+}
