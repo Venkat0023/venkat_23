9bf47c0a829b1a490eb62fc0d9aedcb2c51ccb80
Venkat0023 <41055353+Venkat0023@users.noreply.github.com>
2019-01-17 19:04:56 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Palindrome {
+class Palindrome {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a= scanner.nextInt();
