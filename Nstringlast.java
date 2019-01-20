f012af3916d9c92f16aa22260153a443ce621f7f
Venkat0023 <41055353+Venkat0023@users.noreply.github.com>
2019-01-20 11:36:23 +0530

    Add files via upload

---
+++ Nstringlast.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Created by venkat prasad on 1/20/2019.
+ */
+public class Nstringlast {
+    public static void main(String[] args) {
+        String str,temp="";
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the string");
+        str=scanner.nextLine();
+        System.out.println("Enter the Number");
+        n=scanner.nextInt();
+        for (int i=n;i<=str.length()-1;i++)
+        {
+            temp=temp+str.charAt(i);
+        }
+        System.out.println(temp);
+    }
+}
