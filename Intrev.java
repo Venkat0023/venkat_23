217c8655bde5cebe3f7c2684003e9854837eba3e
Venkat0023 <41055353+Venkat0023@users.noreply.github.com>
2019-01-22 18:36:03 +0530

    Add files via upload

---
+++ Intrev.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Intrev {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a= scanner.nextInt();
+        int rev=0,rem,temp=a;
+        while (temp!=0)
+        {
+            rem=temp%10;
+            rev=rev*10+rem;
+            temp/=10;
+        }
+            System.out.println(rev);
+    }
+}
