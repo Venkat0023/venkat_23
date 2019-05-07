a0dfdb839962f106d6fd8fb17e52a6f6c09d8dfd
Venkat0023 <41055353+Venkat0023@users.noreply.github.com>
2019-05-07 12:42:43 +0530

    Add files via upload

---
+++ Cuboid.java
@@ -0,0 +1,19 @@
+package com.venkat.kids_player;
+
+import java.util.Scanner;
+
+public class Cuboid {
+    public static void main(String[] args) {
+        int l,b,h,sa,vol;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the length,breadth and height");
+        l=scanner.nextInt();
+        b=scanner.nextInt();
+        h=scanner.nextInt();
+        sa=2*(l*b)+2*(b*h)+2*(h*l);
+        vol=l*b*h;
+        System.out.println(sa+"  "+vol);
+
+    }
+
+}
